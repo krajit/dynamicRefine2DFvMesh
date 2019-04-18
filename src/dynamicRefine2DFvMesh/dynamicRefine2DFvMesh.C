aef908e30070b324aed9171b88eb1ceff79b0064
Ajit Kumar <ajitkumar22@gmail.com>
2019-04-19 00:01:01 +0530

    authors updated

--- src/dynamicRefine2DFvMesh/dynamicRefine2DFvMesh.C
+++ src/dynamicRefine2DFvMesh/dynamicRefine2DFvMesh.C
@@ -6,20 +6,13 @@
      \\/     M anipulation  | For copyright notice see file Copyright
 -------------------------------------------------------------------------------
 License
-    This file is part of foam-extend.
+    This file is part of OpenFOAM-v1806.
 
-    foam-extend is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by the
-    Free Software Foundation, either version 3 of the License, or (at your
-    option) any later version.
+Author
+    Luca Cornolti - written for foam-extend
 
-    foam-extend is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+Modified for OpenFOAM-v1806 by
+    Ajit Kumar, Shiv Nadar University, India
 
 
 \*---------------------------------------------------------------------------*/
